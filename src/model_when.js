b13056a2c443f0b040ba27823a6adc8a94510365
Jonathan Eatherly <jonathan.eatherly@gmail.com>
2013-03-31 14:26:46 -0700

    updated immediate promise resolve to pass same paramenters to callbacks as change resolve.

--- src/model_when.js
+++ src/model_when.js
@@ -1,12 +1,13 @@
 (function() {
   _.extend(Backbone.Model.prototype, {
     when: function(attr, value) {
-      var promise = $.Deferred();
+      var promise = $.Deferred(),
+          actualValue = this.get(attr);
 
-      if (value && this.get(attr) === value) {
-        promise.resolve(this);
-      } else if (_.isUndefined(value) && this.get(attr)) {
-        promise.resolve(this);
+      if (value && actualValue === value) {
+        promise.resolve.call(this, this, value);
+      } else if (_.isUndefined(value) && actualValue) {
+        promise.resolve.call(this, this, actualValue);
       }
       this.on("change:" + attr, function(model, newValue) {
         if ((_.isUndefined(value) && newValue) || newValue === value) {
